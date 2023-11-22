4ef725a46b47c170330d0eb1bec341b60ffdfb65
Alina <alinbogz@gmail.com>
2023-11-22 14:33:37 +0100

    initial commit

---
+++ product/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    # Define your URL patterns here
+    # For example:
+    # path('cart/', views.cart_view, name='cart'),
+]
---
+++ user/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+# from .views import home, login_view, signup, user_info
+
+urlpatterns = [
+    # path('', home, name='home'),
+    # path('login/', login_view, name='login'),
+    # path('signup/', signup, name='signup'),
+    # path('userinfo/', user_info, name='userinfo'),
+]
