f520e725b5d7f3b03d510378a0fe337a0184b63a
dom-arm <118741935+dom-arm@users.noreply.github.com>
2023-11-23 16:04:24 +0100

    cart models drafts

--- cart/models.py
+++ cart/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Cart(models.model):
+    """Model representing users cart"""
+
+    # A user can have only one cart and a cart can only belong to one user, so the records in the cart table always need a Unique user foreign key
+    # on_delete=models.RESTRICT prevents the carts associated user being deleted if it is referenced by any cart
+    user = models.ForeignKey("User", on_delete=models.RESTRICT, null=False, unique=True)
+
+    # Can have multiple carditems
+
+    def get_absolute_path(self):
+        pass
+
+
+class CartItem(models.model):
+    """Model representing an item in a cart"""
+
+    # Can only have one cart
+    cart = models.ForeignKey("Cart", on_delete=models.RESTRICT, null=False)
+
+    # Can have multiple instances of a product
