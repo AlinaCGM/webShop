63e06c5943b0251e2decbb845bf1adbcb7969fd8
dom-arm <118741935+dom-arm@users.noreply.github.com>
2023-11-24 11:17:57 +0100

    simple cart view function created

--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from .models import Cart, CartItem
+
+
+def cart_view(request):
+    num_carts = Cart.objects.all().count()
+
+    render(request, "cart.html", context={"num_carts": num_carts})
