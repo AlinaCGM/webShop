3cb461caf745b79667199b2b6bb878fb9245e46c
dom-arm <118741935+dom-arm@users.noreply.github.com>
2023-11-24 16:01:04 +0100

    placeholder product model added for testing

--- product/models.py
+++ product/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+
+# Placeholder for the Product model
+class Product(models.Model):
+    title = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
