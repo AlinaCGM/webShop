8ba529e9ece96273de3727bd03b3a520b6e76115
Alina <alinbogz@gmail.com>
2023-11-24 11:30:53 +0100

    product created

--- product/urls.py
+++ product/urls.py
@@ -1,8 +1,6 @@
 from django.urls import path
-from . import views
+from .views import ProductListView
 
 urlpatterns = [
-    # Define your URL patterns here
-    # For example:
-    # path('cart/', views.cart_view, name='cart'),
+    path('list/', ProductListView.as_view(), name='product_list'),
 ]
