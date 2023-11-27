7da86527f67a867a3e4335fec79a02380b92e859
dom-arm <118741935+dom-arm@users.noreply.github.com>
2023-11-27 10:04:21 +0100

    Test quantity is one as default on cartitem added

--- cart/tests/test_models.py
+++ cart/tests/test_models.py
@@ -3,7 +3,8 @@ from django.test import TestCase
 # Create your tests here.
 
 from django.contrib.auth.models import User
-from cart.models import Cart
+from cart.models import Cart, CartItem
+from product.models import Product
 from django.utils import timezone
 
 
@@ -17,3 +18,17 @@ class CartModelTest(TestCase):
         cart = Cart.objects.create(user=customer)
         created_at = cart.created_at
         self.assertEqual(created_at, timezone.now())
+
+
+class CartItemModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        User.objects.create(username="CustomerIAm")
+
+    def test_quantity_is_one_as_default(self):
+        customer = User.objects.get(id=1)
+        cart = Cart.objects.create(user=customer)
+        product = Product.objects.create(title="Tea cup", price="100")
+        cartitem = CartItem(cart=cart, product=product)
+        quantity = cartitem.quantity
+        self.assertEqual(quantity, 1)
