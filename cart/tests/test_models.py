c3b824587ae2945ef1961b41d96574c4730b4a31
dom-arm <118741935+dom-arm@users.noreply.github.com>
2023-11-28 10:20:00 +0100

    use the new User_Model in tests

--- cart/tests/test_models.py
+++ cart/tests/test_models.py
@@ -2,9 +2,7 @@ from django.test import TestCase
 
 # Create your tests here.
 
-# from django.contrib.auth.models import User
 from user.models import User_Model
-
 from cart.models import Cart, CartItem
 from product.models import Product
 from datetime import date
@@ -13,16 +11,21 @@ from datetime import date
 class CartModelTest(TestCase):
     @classmethod
     def setUpTestData(cls):
-        User_Model.objects.create(username="CustomerIAm")
+        # Create a user on the class
+        cls.user = User_Model.objects.create(
+            email="testuser@example.com", username="testuser"
+        )
+        # Store user_id in a class variable so it easily can be refrenced in the tests
+        cls.user_id = cls.user.user_id
 
     def test_created_at_timestamp_now(self):
-        customer = User_Model.objects.get(id=1)
+        customer = User_Model.objects.get(user_id=self.user_id)
         cart = Cart.objects.create(user=customer)
         created_at = cart.created_at
         self.assertEqual(created_at, date.today())
 
     def test_get_absolute_url(self):
-        customer = User_Model.objects.get(id=1)
+        customer = User_Model.objects.get(user_id=self.user_id)
         cart = Cart.objects.create(user=customer)
         self.assertEqual(cart.get_absolute_url(), "/cart/1/")
 
@@ -30,10 +33,15 @@ class CartModelTest(TestCase):
 class CartItemModelTest(TestCase):
     @classmethod
     def setUpTestData(cls):
-        User_Model.objects.create(username="CustomerIAm")
+        # Create a user on the class
+        cls.user = User_Model.objects.create(
+            email="testuser@example.com", username="testuser"
+        )
+        # Store user_id in a class variable so it easily can be refrenced in the tests
+        cls.user_id = cls.user.user_id
 
     def test_quantity_is_one_as_default(self):
-        customer = User_Model.objects.get(id=1)
+        customer = User_Model.objects.get(user_id=self.user_id)
         cart = Cart.objects.create(user=customer)
         product = Product.objects.create(title="Tea cup", price="100")
         cartitem = CartItem(cart=cart, product=product)
@@ -41,7 +49,7 @@ class CartItemModelTest(TestCase):
         self.assertEqual(quantity, 1)
 
     def test_get_absolute_url(self):
-        customer = User_Model.objects.get(id=1)
+        customer = User_Model.objects.get(user_id=self.user_id)
         cart = Cart.objects.create(user=customer)
         product = Product.objects.create(title="Tea cup", price="100")
         cartitem = CartItem(cart=cart, product=product)
