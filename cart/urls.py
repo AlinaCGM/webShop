e245386d4a23cdcfaa10ba1b5b6da889331da453
dom-arm <118741935+dom-arm@users.noreply.github.com>
2023-11-28 13:27:13 +0100

    changed cart function view to cart class detail view

--- cart/urls.py
+++ cart/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from . import views
+from cart.views import CartDetailView
 
 urlpatterns = [
     # Define your URL patterns here
-    path("<int:pk>/", views.cart, name="cart"),
+    # path("<int:pk>/", views.cart, name="cart"),
+    path("<int:pk>/", CartDetailView.as_view(), name="cart"),
     path(
         "<int:cart_pk>/item/<uuid:item_pk>/",
         views.cart_item_detail,
